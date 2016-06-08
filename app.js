f5671e39f78c6a82456828ff68159ec6bd5a7379
electrostaticfleece <trevorwong00@gmail.com>
2016-06-08 16:31:30 -0700

    Added addtional routes for app.js

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const http = require('http'),
       express = require('express'),
       app = express();
 
-app.get('*', (req, res) => {
+app.all("*", function(req, res, next) {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  next();
+});
+
+app.get('/', (req, res) => {
     var ip = req.ip,
         lang =  req.acceptsLanguages(),
         oppSys = req.headers['user-agent'].match(/\(([^)]+)\)/)[0].slice(1,-1);
     res.end(JSON.stringify({"ipaddress": ip, "language": lang, 'OS': oppSys }));
 });
+
+app.get("*", function(req, res) {
+  res.end("404!");
+});
+
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port " + listener.address().port);
