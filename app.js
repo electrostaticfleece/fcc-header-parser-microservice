2f7b755ef4ba8e0b8ce3b4966c29cd6b720fe1cd
electrostaticfleece <trevorwong00@gmail.com>
2016-06-08 15:37:53 -0700

    Added app and package.json

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http'),
+      express = require('express'),
+      app = express();
+
+app.get('*', (req, res) => {
+    var ip = req.ip,
+        lang =  req.acceptsLanguages(),
+        oppSys = req.headers['user-agent'].match(/\(([^)]+)\)/)[0].slice(1,-1);
+        res.end(JSON.stringify({"ipaddress": ip, "language": lang, 'OS': oppSys }));
+    res.end()
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is listening on port " + listener.address().port);
+});
